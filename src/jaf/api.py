5a9e9520016db2bcfb1e9f19d94a55196ad4ce3d
Kevin Steptoe <kevin.steptoe@gmail.com>
2024-05-20 11:41:45 +0100

    Initial commit

---
+++ src/jaf/api.py
@@ -0,0 +1,53 @@
+# ---- Python API ----
+# The functions defined in this section can be imported by users in their
+# Python scripts/interactive interpreter, e.g. via
+# `from my_test_project.jaf import jaf`,
+# when using this Python module as a library.
+
+import logging
+import sys
+from jaf import __version__
+
+_logger = logging.getLogger(__name__)
+
+
+def setup_logging(loglevel):
+    """Setup basic logging
+
+    Args:
+      loglevel (int): minimum loglevel for emitting messages
+    """
+    logformat = "[%(asctime)s] %(levelname)s:%(name)s:%(message)s"
+    logging.basicConfig(
+        level=loglevel, stream=sys.stdout, format=logformat, datefmt="%Y-%m-%d %H:%M:%S"
+    )
+
+
+def jaf_api(n: int, loglevel: int ):
+    """Wrapper allowing :func: $(package) to be called with string arguments in a CLI fashion
+
+    Args:
+      n: int integer argument to calculate fibonacci
+      loglevel: int
+    """
+    setup_logging(loglevel)
+    _logger.info(f"Version: {__version__}")
+    print("The {}-th Fibonacci number is {}".format(n, jaf(n)))
+    _logger.info("Script ends here")
+
+
+def jaf(n):
+    """jaf provides a Fibonacci example function
+
+    Args:
+      n (int): integer
+
+    Returns:
+      int: n-th Fibonacci number
+    """
+    assert n > 0
+    a, b = 1, 1
+    for _i in range(n - 1):
+        a, b = b, a + b
+    return a
+
